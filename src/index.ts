0236519cee9e7ad41826162dac34b0e8fed3e695
Valkyr97 <develloperjam.97@gmail.com>
2024-02-17 02:40:47 -0500

    fix: tailwind styles

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import './tailwind.css'
+
 export { default as FloatingActionBar } from "./components/bar/FloatingActionBar.vue";
 // export {default as Button} from './components/example/Button.vue'
